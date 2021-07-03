07e6d2c2c9728a62aa461644232f36a529c0b80f
yasar2385 <yasar.mohideen@newgen.co>
2021-07-03 14:34:30 +0530

    3 files modified

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 document.getElementById('');
-
+var Arr = [
+  'circle-border',
+  'circle-border45',
+  'circle-border90',
+  'circle-border135',
+  'circle-border180'
+];
+var svg = document.getElementById('svg');
+for (var index = 1; index <= 3; index++) {
+  (function(index) {
+    setTimeout(function() {
+      svg.classList.forEach(list => {
+        svg.classList.remove(list);
+      });
+      svg.classList.add(Arr[index]);
+    }, index * 1000);
+  })(index);
+}
