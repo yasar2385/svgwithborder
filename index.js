d77c2a6617bf68a7fc9460e135e28036cf0b6a02
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-05-25 10:49:30 +0530

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-//appDiv.innerHTML = `<h1>JS Starter</h1>`;
-document.getElementById('');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
 var Arr = [
   'circle-border',
   'circle-border45',
   'circle-border90',
   'circle-border135',
-  'circle-border180'
+  'circle-border180',
 ];
-var svg = document.getElementById('svg');
-setInterval(() => {
-  Roate();
-}, 4000);
-function Roate() {
-  for (var index = 1; index <= 3; index++) {
-    (function(index) {
-      setTimeout(function() {
-        svg.classList.forEach(list => {
-          svg.classList.remove(list);
-        });
-        svg.classList.add(Arr[index]);
-      }, index * 1000);
-    })(index);
-  }
-}
+// var svg = document.getElementById('svg');
+// setInterval(() => {
+//   Roate();
+// }, 4000);
+// function Roate() {
+//   for (var index = 1; index <= 3; index++) {
+//     (function(index) {
+//       setTimeout(function() {
+//         svg.classList.forEach(list => {
+//           svg.classList.remove(list);
+//         });
+//         svg.classList.add(Arr[index]);
+//       }, index * 1000);
+//     })(index);
+//   }
+// }
